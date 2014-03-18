bec4f558607bafdc913bdaa58bcefffb980f3997
Dmitriy Konovalov <d.konovalov.39@gmail.com>
2014-03-19 01:16:38 +0200

    rich text field doesn't work yet

--- lib/rspec/core/formatters/freemind_formatter.rb
+++ lib/rspec/core/formatters/freemind_formatter.rb
@@ -1,55 +1,159 @@
-#require 'nokogiri'
 require 'time'
 
+
+#class String
+#
+#
+#  def puts_close_node(_)
+#    puts '</node>'
+#  end
+#
+#  def puts_ex_gr_node(notification)
+#    puts "<node TEXT='#{notification.group.description.strip}'"
+#  end
+#
+#  def puts_passed_ex(passed)
+#    puts "<node TEXT='#{passed.example.description.strip}'"
+#    puts "<icon BUILTIN='button_ok'>"
+#    puts "</node>"
+#  end
+#
+#  def puts_failed_ex(failed)
+#    puts "<node TEXT='#{failed.example.description.strip}'"
+#    puts "<icon BUILTIN='button_cancel'>"
+#    puts "</node>"
+#  end
+#
+#  def puts_pending_ex(pending)
+#    puts "<node TEXT='#{pending.example.description.strip}'"
+#    puts "<icon BUILTIN='idea'>"
+#    puts "</node>"
+#  end
+#
+#  def puts_rt_note(duration, example_count, failure_count, pending_count)
+#    puts "<richcontent TYPE='NOTE'><html>"
+#    puts "<head>"
+#    puts "</head>"
+#    puts "<body>"
+#    puts_test_info('test run duration' => '%.2f' % duration)
+#    puts_test_info('example count' => example_count)
+#    puts_test_info('failure count' => failure_count)
+#    puts_test_info('pending count' => pending_count)
+#    puts "</body>"
+#    puts "</html>"
+#    puts "</richcontent>"
+#  end
+#
+#  def puts_test_info(hash)
+#    puts "<p>"
+#    puts hash.keys[0].to_s + ': ' + hash.values[0].to_s
+#    puts "</p>"
+#  end
+#
+#end
+
+# Dumps rspec results as a JUnit XML file.
+# Based on XML schema: http://windyroad.org/dl/Open%20Source/JUnit.xsd
 class RSpec::Core::Formatters::FreeMindFormatter < RSpec::Core::Formatters::BaseFormatter
 
-  def xml
-    @xml ||= Builder::XmlMarkup.new :target => output, :indent => 2
-  end
+
 
   def start example_count
-    @start = Time.now
+    output.puts "<map version='1.0.1'>"    
+  end
+  
+  def example_group_started(notification)
     super
+
+    puts_ex_gr_node(notification)
+  end
+
+  def example_group_finished(_)
+    puts_close_node(_)
+  end
+
+  def example_passed(passed)
+    puts_passed_ex(passed)
+  end
+
+  def example_failed(failed)
+    super
+    puts_failed_ex(failed)
+  end
+
+  def example_pending(pending)
+    super
+    puts_pending_ex(pending)
+  end
+
+  def stop
+    output.puts '</map>'
   end
 
   def dump_summary duration, example_count, failure_count, pending_count
     super
 
-    xml.instruct!
-    xml.testsuite :tests => example_count, :failures => failure_count, :errors => 0, :time => '%.6f' % duration, :timestamp => @start.iso8601 do
-      xml.properties
-      examples.each do |example|
-        send :"dump_summary_example_#{example.execution_result[:status]}", example
-      end
+    #TODO Doesn't work:(
+    lines = File.open(output, 'r') do |file|
+      file.readlines
     end
-  end
-
-  def xml_example example, &block
-    xml.testcase :classname => example_classname(example), :name => example.full_description, :time => '%.6f' % example.execution_result[:run_time], &block
-  end
-
-  def dump_summary_example_passed example
-    xml_example example
-  end
-
-  def dump_summary_example_pending example
-    xml_example example do
-      xml.skipped
+    lines.insert(2, puts_rt_note(duration, example_count, failure_count, pending_count))
+    File.open(output, 'w') do |file|
+      lines = lines.inject {|sum, n| sum.to_s + n.to_s}
+      #lines.gsub!('\'', '\"')
+      file.puts lines
     end
+
+
+
+
   end
 
-  def dump_summary_example_failed example
-    exception = example.execution_result[:exception]
-    backtrace = format_backtrace exception.backtrace, example
-
-    xml_example example do
-      xml.failure :message => exception.to_s, :type => exception.class.name do
-        xml.cdata! "#{exception.message}\n#{backtrace.join "\n"}"
-      end
-    end
+  def puts_close_node(_)
+    output.puts '</node>'
   end
 
-  def example_classname example
-    example.file_path.sub(%r{\.[^/]*\Z}, "").gsub("/", ".").gsub(%r{\A\.+|\.+\Z}, "")
+  def puts_ex_gr_node(notification)
+    output.puts "<node TEXT='#{notification.description.strip}'>"
   end
+
+  def puts_passed_ex(passed)
+    output.puts "<node TEXT='#{passed.description.strip}'>"
+    output.puts "<icon BUILTIN='button_ok'/>"
+    output.puts "</node>"
+  end
+
+  def puts_failed_ex(failed)
+    output.puts "<node TEXT='#{failed.description.strip}'>"
+    output.puts "<icon BUILTIN='button_cancel'/>"
+    output.puts "</node>"
+  end
+
+  def puts_pending_ex(pending)
+    output.puts "<node TEXT='#{pending.description.strip}'>"
+    output.puts "<icon BUILTIN='idea'/>"
+    output.puts "</node>"
+  end
+
+  def puts_rt_note(duration, example_count, failure_count, pending_count)
+    output.puts "<richcontent TYPE='NOTE'><html>"
+    output.puts "<head>"
+    output.puts "</head>"
+    output.puts "<body>"
+    puts_test_info('test run duration' => '%.2f' % duration)
+    puts_test_info('example count' => example_count)
+    puts_test_info('failure count' => failure_count)
+    puts_test_info('pending count' => pending_count)
+    output.puts "</body>"
+    output.puts "</html>"
+    output.puts "</richcontent>"
+  end
+
+  def puts_test_info(hash)
+    output.puts "<p>"
+    output.puts hash.keys[0].to_s + ': ' + hash.values[0].to_s
+    output.puts "</p>"
+  end
+
 end
+
--- rspec_freemind_formatter.rb
+++ rspec_freemind_formatter.rb
@@ -1,9 +1,10 @@
 require 'nokogiri'
 require 'builder'
 require 'rspec'
+require 'nokogiri'
 
 require 'rspec/core/formatters/base_formatter'
-require 'rspec/core/formatters/freemind_formatter'
+require_relative 'lib/rspec/core/formatters/freemind_formatter'
 
 
 RspecFreeMindFormatter = RSpecFreeMindFormatter = RSpec::Core::Formatters::FreeMindFormatter
