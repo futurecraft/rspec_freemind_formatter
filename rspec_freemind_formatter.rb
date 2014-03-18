ac6c57da186279221e9feafdf18d55872f8d05db
Dmitriy Konovalov <d.konovalov.39@gmail.com>
2014-03-18 18:38:07 +0200

    initial commit

---
+++ rspec_freemind_formatter.rb
@@ -0,0 +1,9 @@
+require 'nokogiri'
+require 'builder'
+require 'rspec'
+
+require 'rspec/core/formatters/base_formatter'
+require 'rspec/core/formatters/freemind_formatter'
+
+
+RspecFreeMindFormatter = RSpecFreeMindFormatter = RSpec::Core::Formatters::FreeMindFormatter
